b30bbb8275ae45dd2e371bd7b08377b161f6d439
davidemvf <davidemv.fiore@gmail.com>
2019-08-28 19:48:41 +0200

    implementata funzione post aggiunge le caselle solo riaggiornando la pagina

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ function init() {
     console.log("Hello World");
 }
 
+$(document).ready(init);
+
 $.ajax({
   url: "http://157.230.17.132:3007/todos",
   method: "GET",
@@ -9,38 +11,58 @@ $.ajax({
   success: function (data){
     console.log(data)
     // var list = data.results;
-    var source   = document.getElementById("item-template").innerHTML;
-    var template = Handlebars.compile(source);
-
-    for (var i=0; i<data.length; i++) {
-      var item = data[i];
-      var context = {text: item.text, id: item.id};
-      var html    = template(context);
-
-      $(".container").append(html);
-    }
+    printItem(data);
   },
   error: function() {
     alert("Errore")
   }
 })
 
- $(document).on("click", ".esc", deleteItem);
+  $(document).on("click", ".esc", deleteItem);
 
- function deleteItem() {
-   var this_element = $(this);
-   var elementToDel = this_element.parent();
-   var id_element = elementToDel.data("id");
+  function deleteItem() {
+    var this_element = $(this);
+    var elementToDel = this_element.parent();
+    var id_element = elementToDel.data("id");
 
-   $.ajax({
-     url: "http://157.230.17.132:3007/todos/" + id_element,
-     method: "DELETE",
-     success: function () {
-       console.log("Cancellato item" + id_element);
-       elementToDel.remove();
-     }
+    $.ajax({
+      url: "http://157.230.17.132:3007/todos/" + id_element,
+      method: "DELETE",
+      success: function () {
+        console.log("Cancellato item" + id_element);
+        elementToDel.remove();
+      }
 
-   })
- }
+    })
+  }
 
-$(document).ready(init);
+  $(document).on("click", "#add-item", addItem);
+
+  // $("#add-item").click(addItem);
+
+  function addItem() {
+    var text = $("#text-item").val();
+
+    $.ajax({
+      url: "http://157.230.17.132:3007/todos/",
+      method: "POST",
+      data: {text: text},
+      success: function(data){
+        printItem(data);
+      }
+    })
+
+  };
+
+  function printItem (items) {
+    var source   = document.getElementById("item-template").innerHTML;
+    var template = Handlebars.compile(source);
+
+    for (var i=0; i<items.length; i++) {
+      var item = items[i];
+      var context = {text: item.text, id: item.id};
+      var html    = template(context);
+
+      $(".container").append(html);
+    }
+  };
